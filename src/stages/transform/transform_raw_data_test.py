cc8e1c7efb99283fa6f69f775679107fa0648098
0xK4z <126207199+0xK4z@users.noreply.github.com>
2026-04-25 14:17:50 -0300

    test(transform): update assertions for TransformContract

--- src/stages/transform/transform_raw_data_test.py
+++ src/stages/transform/transform_raw_data_test.py
@@ -1,4 +1,5 @@
 from src.drivers.mocks.http_collector_mock import mock_extract_contract
+from src.stages.contracts.transform_contract import TransformContract
 from src.stages.transform.transform_raw_data import TransformRawData
 
 
@@ -8,10 +9,10 @@ def test_transform():
     transform_raw_data = TransformRawData()
     transformed_information = transform_raw_data.transform(extract_contract)
 
-    assert isinstance(transformed_information, list)
-    assert isinstance(transformed_information[0], dict)
-    assert transformed_information[0]["first_name"] == "John"
-    assert transformed_information[0]["last_name"] == "Smith"
+    assert isinstance(transformed_information, TransformContract)
+    assert transformed_information.load_content[0]["first_name"] == "John"
+    assert transformed_information.load_content[0]["last_name"] == "Smith"
     assert (
-        transformed_information[0]["link"] == "https://example.com/artist?artistid=123"
+        transformed_information.load_content[0]["link"]
+        == "https://example.com/artist?artistid=123"
     )
