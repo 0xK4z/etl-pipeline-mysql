c80aad33cc40d5d4876a9a163ee117b6dc4a6451
0xK4z <126207199+0xK4z@users.noreply.github.com>
2026-04-25 14:20:00 -0300

    test(transform): add error handling test for TransformRawData

--- src/stages/transform/transform_raw_data_test.py
+++ src/stages/transform/transform_raw_data_test.py
@@ -1,4 +1,5 @@
 from src.drivers.mocks.http_collector_mock import mock_extract_contract
+from src.errors.transform_error import TransformError
 from src.stages.contracts.transform_contract import TransformContract
 from src.stages.transform.transform_raw_data import TransformRawData
 
@@ -16,3 +17,15 @@ def test_transform():
         transformed_information.load_content[0]["link"]
         == "https://example.com/artist?artistid=123"
     )
+
+
+def test_transform_error():
+    extract_contract = mock_extract_contract()
+    extract_contract.raw_information_content[0]["link"] = "invalid_link"
+
+    transform_raw_data = TransformRawData()
+
+    try:
+        transform_raw_data.transform(extract_contract)
+    except Exception as exception:  # pylint: disable=broad-exception-caught
+        assert isinstance(exception, TransformError)
