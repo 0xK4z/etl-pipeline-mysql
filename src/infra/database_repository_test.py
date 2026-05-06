fc402713bd4251650198a18f990a2a1718bbe53d
0xK4z <126207199+0xK4z@users.noreply.github.com>
2026-05-06 16:53:38 -0300

    test: skip insert_artist test requiring real database connection

--- src/infra/database_repository_test.py
+++ src/infra/database_repository_test.py
@@ -1,6 +1,10 @@
+# pylint: disable=line-too-long
+import pytest
+
 from src.infra.database_repository import DatabaseRepository
 from src.stages.transform.mocks.mock_transform_raw_data import mock_transform_raw_data
 
+@pytest.mark.skip(reason="This test requires a real database connection and should be run in an integration testing environment.")
 def test_insert_artist():
     database_repository = DatabaseRepository()
     transformed_data = mock_transform_raw_data()
